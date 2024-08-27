512c271f382afa6f5e4b4478f6acb9979d95575f
GregoireGeoffroy <thegathering92@gmail.com>
2024-08-27 21:43:30 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,21 @@
 const express = require("express");
+const models = require("./models");
 const app = express();
 const cors = require("cors");
 require('dotenv').config();
 const { v4: uuidv4 } = require('uuid');
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-let users = {
-  1: {
-    id: '1',
-    username: 'Robin Wieruch',
-  },
-  2: {
-    id: '2',
-    username: 'Dave Davids',
-  },
-};
-
-let messages = {
-  1: {
-    id: '1',
-    text: 'Hello World',
-    userId: '1',
-  },
-  2: {
-    id: '2',
-    text: 'By World',
-    userId: '2',
-  },
-};
+app.use((req, res, next) => {
+  req.context = {
+    models,
+    me: models.users[1],
+  };
+  next();
+});
 
 app.get('/users', (req, res) => {
   return res.send(Object.values(users));
@@ -36,6 +23,18 @@ app.get('/users', (req, res) => {
 
 app.get('/users/:userId', (req, res) => {
   return res.send(users[req.params.userId]);
+});
+
+app.post('/messages', (req, res) => {
+  const id = uuidv4();
+  const message = {
+    id,
+    text: req.body.text,
+  };
+
+  messages[id] = message;
+
+  return res.send(message);
 });
 
 app.listen(process.env.PORT, () => console.log(`My first Express app - listening on port ${process.env.PORT}!`));
