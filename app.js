59e67d3619b82587d94110d424265ba17eb5d8d2
GregoireGeoffroy <thegathering92@gmail.com>
2024-08-27 21:20:54 +0300

    add curl methods

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ require('dotenv').config();
 
 app.use(cors());
 
-app.get("/", (req, res) => res.send("Hello, world!"));
+app.get('/', (req, res) => {
+  return res.send('Received a GET HTTP method');
+});
 
+app.post('/', (req, res) => {
+  return res.send('Received a POST HTTP method');
+});
+
+app.put('/', (req, res) => {
+  return res.send('Received a PUT HTTP method');
+});
+
+app.delete('/', (req, res) => {
+  return res.send('Received a DELETE HTTP method');
+});
 app.listen(process.env.PORT, () => console.log(`My first Express app - listening on port ${process.env.PORT}!`));
