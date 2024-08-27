3f5f40b0b530e79194b006a9b92e2f51c6ce2d7b
GregoireGeoffroy <thegathering92@gmail.com>
2024-08-27 21:32:55 +0300

    add uuid

--- app.js
+++ app.js
@@ -2,22 +2,40 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require('dotenv').config();
+const { v4: uuidv4 } = require('uuid');
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-  return res.send('Received a GET HTTP method');
+let users = {
+  1: {
+    id: '1',
+    username: 'Robin Wieruch',
+  },
+  2: {
+    id: '2',
+    username: 'Dave Davids',
+  },
+};
+
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'By World',
+    userId: '2',
+  },
+};
+
+app.get('/users', (req, res) => {
+  return res.send(Object.values(users));
 });
 
-app.post('/', (req, res) => {
-  return res.send('Received a POST HTTP method');
+app.get('/users/:userId', (req, res) => {
+  return res.send(users[req.params.userId]);
 });
 
-app.put('/', (req, res) => {
-  return res.send('Received a PUT HTTP method');
-});
-
-app.delete('/', (req, res) => {
-  return res.send('Received a DELETE HTTP method');
-});
 app.listen(process.env.PORT, () => console.log(`My first Express app - listening on port ${process.env.PORT}!`));
